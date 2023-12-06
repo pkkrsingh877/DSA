423dee41b7e25a9043d7beede02fb2d139b02134
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-06 22:41:38 +0530

    SLL : Insert at index i

---
+++ Linked Lists/Singly Linked Lists/Insertion at index i/main.c
@@ -0,0 +1,101 @@
+// Program to insert at index i of linked list where i is not 0 and n
+// Here 0 is the first index and n is the last index
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    struct node{
+        int data;
+        struct node *next;
+    };
+
+    int choice;
+    int index;
+
+    struct node *head = NULL;
+    struct node *newnode = NULL;
+    struct node *temp = NULL;
+    struct node *tail = NULL;
+
+    do{
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        printf("Enter data: ");
+        scanf("%d", &newnode -> data);
+        newnode -> next = NULL;
+
+        if(head == NULL){
+            head = temp = newnode;
+        }else{
+            temp -> next = newnode;
+            temp = newnode;
+            tail = newnode;
+        }
+
+        printf("Do you want to continue(0:Break): ");
+        scanf("%d",&choice);
+    }while(choice);
+
+    // Here get input to store index at which node has to be inserted
+
+    printf("Enter Index: ");
+    scanf("%d", &index);
+
+    printf("Traversing Linked List ...\n\n");
+
+    temp = head;
+    int count = -1; // Count total number of elements in array
+
+    while(temp != NULL){
+        printf("%d\n", temp -> data);
+        temp = temp -> next;
+        count++;
+    }
+
+    // Here we can check if location entered by user is not invalid
+    if(index < 1 || index > count){
+        printf("Invalid Location!\n\n");
+    }else{
+        int traverseIndex = 0;
+        printf("Traversing to update ...\n\n");
+
+        temp = head;
+
+        while(temp != NULL){
+            if(traverseIndex == index - 1){
+                // Create a newnode
+                newnode = (struct node*)malloc(sizeof(struct node));
+    
+                printf("Enter data: ");
+                scanf("%d", &newnode -> data);
+                newnode -> next = temp -> next;
+                temp -> next = newnode;
+            }
+            traverseIndex++;
+            temp = temp -> next;
+        }
+    }
+
+    printf("Traversing Linked List After insertion ...\n\n");
+
+    temp = head;
+
+    while(temp != NULL){
+        printf("%d\n", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("About to free memory of all nodes ...\n\n");
+
+    temp = head;
+
+    while(temp != NULL){
+        struct node *currentnode = temp;
+        printf("Free the memory of %d ...\n", currentnode -> data);
+        temp = temp -> next;
+        free(currentnode);
+    }
+
+    return 0;
+}
